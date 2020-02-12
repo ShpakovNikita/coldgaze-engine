57ed43a992431c8f9503eac4cbc530a13eb80a2e
shpakov_nikita <shpakovnikita1998@gmail.com>
2020-02-12 21:48:39 +0300

    vertex and index buffers filled with data on local device

--- coldgaze/src/framework/Core/Private/TriangleEngine.cpp
+++ coldgaze/src/framework/Core/Private/TriangleEngine.cpp
@@ -60,13 +60,13 @@ void CG::TriangleEngine::PrepareVertices()
 
 	std::vector<uint32_t> indexBuffer = { 0, 1, 2 };
 	indices.count = static_cast<uint32_t>(indexBuffer.size());
-	// uint32_t indexBufferSize = indices.count * sizeof(uint32_t);
+	uint32_t indexBufferSize = indices.count * sizeof(uint32_t);
 
 	VkMemoryAllocateInfo memAlloc = {};
 	memAlloc.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
 	VkMemoryRequirements memReqs;
 
-	// void* data;
+	void* data;
 
 	struct StagingBuffer {
 		VkDeviceMemory memory;
@@ -87,6 +87,62 @@ void CG::TriangleEngine::PrepareVertices()
 	VK_CHECK_RESULT(vkCreateBuffer(device, &vertexBufferInfo, nullptr, &stagingBuffers.vertices.buffer));
 	vkGetBufferMemoryRequirements(device, stagingBuffers.vertices.buffer, &memReqs);
 	memAlloc.allocationSize = memReqs.size;
-
 	memAlloc.memoryTypeIndex = GetMemoryTypeIndex(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+	VK_CHECK_RESULT(vkAllocateMemory(device, &memAlloc, nullptr, &stagingBuffers.vertices.memory));
+	VK_CHECK_RESULT(vkMapMemory(device, stagingBuffers.vertices.memory, 0, memAlloc.allocationSize, 0, &data));
+	memcpy(data, vertexBuffer.data(), vertexBufferSize);
+	vkUnmapMemory(device, stagingBuffers.vertices.memory);
+	VK_CHECK_RESULT(vkBindBufferMemory(device, stagingBuffers.vertices.buffer, stagingBuffers.vertices.memory, 0));
+
+	vertexBufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+	VK_CHECK_RESULT(vkCreateBuffer(device, &vertexBufferInfo, nullptr, &vertices.buffer));
+	vkGetBufferMemoryRequirements(device, vertices.buffer, &memReqs);
+	memAlloc.allocationSize = memReqs.size;
+	memAlloc.memoryTypeIndex = GetMemoryTypeIndex(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+	VK_CHECK_RESULT(vkAllocateMemory(device, &memAlloc, nullptr, &vertices.memory));
+	VK_CHECK_RESULT(vkBindBufferMemory(device, vertices.buffer, vertices.memory, 0));
+
+	VkBufferCreateInfo indexbufferInfo = {};
+	indexbufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	indexbufferInfo.size = indexBufferSize;
+	indexbufferInfo.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+	VK_CHECK_RESULT(vkCreateBuffer(device, &indexbufferInfo, nullptr, &stagingBuffers.indices.buffer));
+	vkGetBufferMemoryRequirements(device, stagingBuffers.indices.buffer, &memReqs);
+	memAlloc.allocationSize = memReqs.size;
+	memAlloc.memoryTypeIndex = GetMemoryTypeIndex(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+	VK_CHECK_RESULT(vkAllocateMemory(device, &memAlloc, nullptr, &stagingBuffers.indices.memory));
+	VK_CHECK_RESULT(vkMapMemory(device, stagingBuffers.indices.memory, 0, indexBufferSize, 0, &data));
+	memcpy(data, indexBuffer.data(), indexBufferSize);
+	vkUnmapMemory(device, stagingBuffers.indices.memory);
+	VK_CHECK_RESULT(vkBindBufferMemory(device, stagingBuffers.indices.buffer, stagingBuffers.indices.memory, 0));
+
+	indexbufferInfo.usage = VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+	VK_CHECK_RESULT(vkCreateBuffer(device, &indexbufferInfo, nullptr, &indices.buffer));
+	vkGetBufferMemoryRequirements(device, indices.buffer, &memReqs);
+	memAlloc.allocationSize = memReqs.size;
+	memAlloc.memoryTypeIndex = GetMemoryTypeIndex(memReqs.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+	VK_CHECK_RESULT(vkAllocateMemory(device, &memAlloc, nullptr, &indices.memory));
+	VK_CHECK_RESULT(vkBindBufferMemory(device, indices.buffer, indices.memory, 0));
+
+	VkCommandBuffer copyCmd = drawCmdBuffers[0];
+
+	// start using command buffers
+	VkCommandBufferBeginInfo cmdBufferBeginInfo{};
+	cmdBufferBeginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+	VK_CHECK_RESULT(vkBeginCommandBuffer(copyCmd, &cmdBufferBeginInfo));
+
+	// Vertex buffer
+	VkBufferCopy copyRegion = {};
+	copyRegion.size = vertexBufferSize;
+	vkCmdCopyBuffer(copyCmd, stagingBuffers.vertices.buffer, vertices.buffer, 1, &copyRegion);
+	// Index buffer
+	copyRegion.size = indexBufferSize;
+	vkCmdCopyBuffer(copyCmd, stagingBuffers.indices.buffer, indices.buffer, 1, &copyRegion);
+
+	// TODO: add flushing of command buffer
+
+	vkDestroyBuffer(device, stagingBuffers.vertices.buffer, nullptr);
+	vkFreeMemory(device, stagingBuffers.vertices.memory, nullptr);
+	vkDestroyBuffer(device, stagingBuffers.indices.buffer, nullptr);
+	vkFreeMemory(device, stagingBuffers.indices.memory, nullptr);
 }
